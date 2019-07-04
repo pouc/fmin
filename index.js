fc4b8cc5aa02e57f862785d906bb8ae92a512ae5
Loïc Formont <loic.formont@gmail.com>
2019-07-04 09:53:11 +0200

    changed export

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-export {bisect} from "./src/bisect";
-export {nelderMead} from "./src/nelderMead";
-export {conjugateGradient, conjugateGradientSolve} from "./src/conjugateGradient";
-export {gradientDescent, gradientDescentLineSearch} from "./src/gradientDescent";
-export {zeros, zerosM, norm2, weightedSum, scale} from "./src/blas1";
+import {bisect} from "./src/bisect";
+import {nelderMead} from "./src/nelderMead";
+import {conjugateGradient, conjugateGradientSolve} from "./src/conjugateGradient";
+import {gradientDescent, gradientDescentLineSearch} from "./src/gradientDescent";
+import {zeros, zerosM, norm2, weightedSum, scale} from "./src/blas1";
+
+export default({
+    bisect: bisect,
+    nelderMead: nelderMead,
+    conjugateGradient: conjugateGradient,
+    conjugateGradientSolve: conjugateGradientSolve,
+    gradientDescent: gradientDescent,
+    gradientDescentLineSearch: gradientDescentLineSearch,
+    zeros: zeros,
+    zerosM: zerosM,
+    norm2: norm2,
+    weightedSum: weightedSum,
+    scale: scale
+});
