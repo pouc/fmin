4643f7fb4b67ccd6bcb7b51d0a1938d0d66dbbf5
Loïc Formont <loic.formont@gmail.com>
2019-07-05 13:57:43 +0200

    export syntax changed

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ import {conjugateGradient, conjugateGradientSolve} from "./src/conjugateGradient
 import {gradientDescent, gradientDescentLineSearch} from "./src/gradientDescent";
 import {zeros, zerosM, norm2, weightedSum, scale} from "./src/blas1";
 
-export default({
-    bisect: bisect,
-    nelderMead: nelderMead,
-    conjugateGradient: conjugateGradient,
-    conjugateGradientSolve: conjugateGradientSolve,
-    gradientDescent: gradientDescent,
-    gradientDescentLineSearch: gradientDescentLineSearch,
-    zeros: zeros,
-    zerosM: zerosM,
-    norm2: norm2,
-    weightedSum: weightedSum,
-    scale: scale
-});
+export {
+    bisect,
+    nelderMead,
+    conjugateGradient,
+    conjugateGradientSolve,
+    gradientDescent,
+    gradientDescentLineSearch,
+    zeros,
+    zerosM,
+    norm2,
+    weightedSum,
+    scale
+};
